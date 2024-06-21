f049e1fbb879b11cb3b95bb2a0dcdb19bb0951de
Alina Mordovina <alina182@yandex.ru>
2024-06-22 02:18:14 +0400

    add admin materials

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from materials.models import Section, Material
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ("id", "title",)
+    search_fields = (
+        "title",
+        "description",
+    )
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ("id", "title",)
+    list_filter = ("section",)
+    search_fields = (
+        "title",
+        "description",
+    )
